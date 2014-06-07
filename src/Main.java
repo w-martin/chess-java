6b8651b3315738f5e7b406cb18db36d4533f2a92
William Martin <will.st4@gmail.com>
2014-06-07 12:04:59 +0100

    Implemented Queen.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.chess.model.Position;
 import com.chess.standard.StandardBoard;
 import com.chess.standard.StandardPosition;
 import com.chess.standard.pieces.Bishop;
-import com.chess.standard.pieces.Rook;
+import com.chess.standard.pieces.Queen;
 
 import java.util.Set;
 
@@ -17,7 +17,9 @@ public class Main {
         StandardBoard b = new StandardBoard();
         b.addPieceToPosition(StandardPosition.positionFromCoord("F4"), new Bishop(0));
         b.addPieceToPosition(StandardPosition.positionFromCoord("G1"), new Bishop(1));
-        Set<Position> positions = new Rook(0).computePossibleMoves(StandardPosition.positionFromCoord("F1"), b);
+        b.addPieceToPosition(StandardPosition.positionFromCoord("D1"), new Bishop(1));
+        b.addPieceToPosition(StandardPosition.positionFromCoord("E2"), new Bishop(1));
+        Set<Position> positions = new Queen(0).computePossibleMoves(StandardPosition.positionFromCoord("F1"), b);
 //        Collections.sort(new ArrayList<Position>(positions.));
         for (Position p : positions) {
 //            System.out.println(p.toString());
